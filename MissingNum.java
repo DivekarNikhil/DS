181a2590e6f013fc0e79a1ecedf8d15231c18e15
DivekarNikhil <nikhildivekar2002@gmail.com>
2024-07-28 10:00:14 +0530

    Missing Number Find

--- MissingNum.java
+++ MissingNum.java
@@ -1,21 +1,20 @@
+import java.util.Scanner;
+
 public class MissingNum
 {
     public static void main(String[] args)
     {
-        int a[]={1,2,4,5,6};
-
-
-            for(int j=0;j<=a[];j++)
-            {
-                if(a[i]!=j)
-                {
-                    System.out.println("Missing Element is"+j);
-                }
-
-
-        }
-
+        Scanner sc=new Scanner(System.in);
+      int a[]={1,2,4,5};
+      int n= a.length+1;
+      int sum=(n*(n+1)/2);
+      for(int i=0;i<a.length;i++)
+      {
+          sum=sum-a[i];
+      }
+        System.out.println("Missing Number is : "+sum);
 
 
     }
+
 }
